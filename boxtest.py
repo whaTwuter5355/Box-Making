ba24d696e30738e426cb770514279cf350b5ea88
whaTwuter5355 <trenton.himel@gmail.com>
2026-07-07 21:08:44 -0500

    Add files via upload
    my first box, made it resizable!

---
+++ boxtest.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.title("Blue Box")
+window.geometry("500x300")
+window.resizable(True, True) 
+window.configure(bg="#05a3ff")
+
+window.mainloop()
